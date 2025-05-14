f29c88bc8ccbbfd22ee86e07646eb32740407a94
sandramarincodes <smarinfernan@gmail.com>
2025-05-14 20:12:09 +0200

    Update buscando productos completed

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,28 @@
 "use strict";
 
-// Sección de variables globales (datos)
+// SECCIÓN DE VARIANLES GLOBALES (DATOS)
 let url = "https://fakestoreapi.com/products";
+let storeProducts = [];
+
 // Sección de query-selectors (elementos que usamos de la página)
 const ulList = document.querySelector(".js_list");
+const inputSearch = document.querySelector(".js_input-search");
+const btnSearch = document.querySelector(".js_btn-search");
 
-/* const product1 = {
-  id: 1,
-  title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-  price: 109.95,
-  description:
-    "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-  category: "men's clothing",
-  image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-  rating: {
-    rate: 3.9,
-    count: 120,
-  },
-}; */
-// Sección de funciones
+// SECCIÓN DE FUNCIONES
 
 function renderProduct(product) {
-  return `<li><div><img src="${product.image}" style="width: 200px;"/>
+  let imgURL;
+  if (product.image === "") {
+    imgURL = "https://placehold.co/600x400";
+  } else {
+    imgURL = product.image;
+  }
+
+  return `<li><div><img src="${imgURL}" style="width: 200px;"/>
     <p>${product.title}</p><p>${product.price}</p>
     <button>Comprar</button></div></li>`;
-} // DUDA ANTES LO TENIA ASÍ ulList.innerHtml += `<li>... (Comprobé que sí que pintaba. Lo he pasado a return porque siento que luego la usaré. Tiene sentido?)
-
-/* renderProduct(product1); */
+} // DUDA ANTES LO TENIA ASÍ ulList.innerHtml = `<li>... (Comprobé que sí que pintaba. Lo he pasado a return porque siento que luego la usaré. Tiene sentido?)
 
 function renderAllProducts(productList) {
   ulList.innerHTML = "";
@@ -39,11 +35,23 @@ function getProducts() {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-        renderAllProducts(data)
+      storeProducts = data;
+      renderAllProducts(storeProducts);
     });
 }
 
-getProducts()
 // Sección de funciones de eventos
+
+function handleSearch(event) {
+  event.preventDefault();
+  let valueSearch = inputSearch.value;
+  const filteredProducts = storeProducts.filter((product) =>
+    product.title.toLowerCase().includes(valueSearch.toLowerCase())
+  );
+  console.log(filteredProducts);
+  renderAllProducts(filteredProducts);
+}
 // Sección de eventos
+btnSearch.addEventListener("click", handleSearch);
 // Sección de código a ejecutar cuando carga la página
+getProducts();
