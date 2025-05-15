107f7560d533ccfa4ae75727a0fb7ac111762b9a
sandramarincodes <smarinfernan@gmail.com>
2025-05-15 16:53:15 +0200

    Update añadir estilos tras clic botón comprar

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,14 @@
 // SECCIÓN DE VARIANLES GLOBALES (DATOS)
 let url = "https://fakestoreapi.com/products";
 let storeProducts = [];
+let shoppingCart = [];
+let currentProductList = [];
 
 // Sección de query-selectors (elementos que usamos de la página)
 const ulList = document.querySelector(".js_list");
 const inputSearch = document.querySelector(".js_input-search");
 const btnSearch = document.querySelector(".js_btn-search");
+const ulShopping = document.querySelector(".js_shopping-cart");
 
 // SECCIÓN DE FUNCIONES
 
@@ -19,15 +22,41 @@ function renderProduct(product) {
     imgURL = product.image;
   }
 
-  return `<li><div><img src="${imgURL}" style="width: 200px;"/>
+  let isOnCart = false;
+  for (const item of shoppingCart) {
+    if (item.id === product.id) {
+      isOnCart = true;
+    }
+  }
+
+  let divClass;
+  if (isOnCart) {
+    divClass = "selected-card";
+  } else {
+    divClass = "card";
+  }
+
+  let buttonText;
+  if (isOnCart) {
+    buttonText = "Eliminar";
+  } else {
+    buttonText = "Comprar";
+  }
+  return `<li><div class="${divClass}"><img src="${imgURL}" style="width: 200px;"/>
     <p>${product.title}</p><p>${product.price}</p>
-    <button>Comprar</button></div></li>`;
-} // DUDA ANTES LO TENIA ASÍ ulList.innerHtml = `<li>... (Comprobé que sí que pintaba. Lo he pasado a return porque siento que luego la usaré. Tiene sentido?)
+    <button class="js_btn-cart" id="${product.id}">${buttonText}</button></div></li>`;
+}
 
 function renderAllProducts(productList) {
+  currentProductList = productList;
   ulList.innerHTML = "";
   for (const product of productList) {
     ulList.innerHTML += renderProduct(product);
+  }
+  // al principio lo puse esto fuera pero no tiene sentido porque los botones aún no existen no? al principio puse queryselector normal y solo seleeccionaba un elemento.
+  const btnsCart = document.querySelectorAll(".js_btn-cart");
+  for (const btn of btnsCart) {
+    btn.addEventListener("click", handleAddToCart);
   }
 }
 
@@ -51,7 +80,21 @@ function handleSearch(event) {
   console.log(filteredProducts);
   renderAllProducts(filteredProducts);
 }
+
+function handleAddToCart(event) {
+  event.preventDefault();
+  const clickedId = parseInt(event.currentTarget.id);
+  const productToCart = storeProducts.find(
+    (product) => product.id === clickedId
+  );
+  if (!shoppingCart.find((item) => item.id === productToCart.id)) {
+    shoppingCart.push(productToCart);
+    renderAllProducts(currentProductList);
+  }
+}
+
 // Sección de eventos
 btnSearch.addEventListener("click", handleSearch);
+
 // Sección de código a ejecutar cuando carga la página
 getProducts();
